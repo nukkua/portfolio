76906d6c504610b6faba615a10a8651fdb5bf7a8
nukkua <marco.salazar.2014.com@gmail.com>
2023-10-25 15:15:16 -0400

    finished:portflio

---
+++ src/components/Footer.svelte
@@ -0,0 +1,31 @@
+<footer class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center">
+	<p class="px-4 py-2 bg-white text-slate-950 font-medium">
+		Connect with me &darr;
+	</p>
+
+	<div class="flex flex-col gap-4 items-center justify-center">
+		<p><strong class="pr-2">Email</strong></p>
+		<p>
+			<strong class="pr-2">Github</strong>
+			<a href="https://github.com/nukkua" target="_blank" class="text-violet-400">Nukkua
+				<sup class="">
+					<span class="text-xs scale-75 pl-0.5">
+						<i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"></i>
+					</span>
+				</sup>
+			</a>
+		</p>
+
+		<p>
+			<strong class="pr-2">LinkedIn</strong>
+			<a href="https://www.linkedin.com/in/marco-antonio-justiniano-salazar-79794427b/" target="_blank" class="text-violet-400">Marco Antonio Justiniano Salazar
+				<sup>
+					<span class="text-xs scale-75 pl-0.5">
+						<i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"> </i>
+					</span>
+				</sup>
+			</a>
+		</p>
+	</div>
+	
+</footer>
